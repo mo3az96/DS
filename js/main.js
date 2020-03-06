30da29fcab0376832bec2f36c16abc0f0627100b
mo3az <51883008+mo3az96@users.noreply.github.com>
2020-03-06 18:58:52 +0200

    contact

--- js/main.js
+++ js/main.js
@@ -7,6 +7,41 @@ $(window).on("load", function () {
         }, 1);
         new WOW().init();
     });
+
+
+    //map
+    var adresse = "";
+
+
+    var location = [adresse[0], $("#map").attr("lat"), $("#map").attr("long")];
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 8,
+        overviewMapControl: false,
+
+        center: new google.maps.LatLng(24.8629889, 46.5980617),
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        zoomControl: false,
+        fullscreenControl: false
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker;
+    marker = new google.maps.Marker({
+        position: new google.maps.LatLng(location[1], location[2]),
+        map: map,
+        icon: "images/pin.png"
+    });
+
+    google.maps.event.addListener(marker, 'click', (function (marker, i) {
+        return function () {
+            infowindow.setContent(location[0]);
+            infowindow.open(map, marker);
+        }
+    })(marker));
+    /////////////////////////////////////
+
 });
 $(document).ready(function () {
     /////////Main Slider/////////
